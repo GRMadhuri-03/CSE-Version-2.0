79e516ca1dd4460fe4aa53a243d6eb42283aa2f8
GRMadhuri-03 <124073241+GRMadhuri-03@users.noreply.github.com>
2023-05-26 22:10:43 +0530

    4 files modified, 2 added

--- src/components/pages/VisionAndMission/VisionAndMission.jsx
+++ src/components/pages/VisionAndMission/VisionAndMission.jsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './VisionAndMission.css';
+import { Link } from 'react-router-dom';
+import Drop from './Drop';
+import './Drop.css';
 export default function VisionAndMission() {
+  const [publishs, setpublish] = useState([
+    {
+      
+    },
+  ]);
+  const toggledrop = (index) => {
+    setpublish(
+      publishs.map((publish, i) => {
+        if (i === index) {
+          publish.open = !publish.open;
+        } else {
+          publish.open = false;
+        }
+
+        return publish;
+      })
+    );
+  };
+
   return (
     <>
-      <h1 className="consulting">VisionAndMission</h1>
+      {/* <Navbar /> */}
+      <div className="main-box">
+        <div className="side-box">
+          <div className="side-container">
+            <div className="side-top-box">
+              <p>About US</p>
+            </div>
+
+            <ul className="side-link">
+              <li>
+                <div className="side-border-left" />
+                <Link to="/VisionAndMission">History</Link>
+              </li>
+              <li>
+                <div className="side-border-left" />
+                <Link to="/VisionAndMission">Vision And Mission</Link>
+              </li>
+              <li>
+                {' '}
+                <div className="side-border-left" />
+                <Link to="/POs">POs,PEOs,PSOs</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="drops">
+          <h3>Vision</h3>
+          {publishs.map((publish, index) => (
+            <Drop
+              publish={publish}
+              index={index}
+              key={index}
+              toggledrop={toggledrop}
+            />
+          ))}
+        </div>
+      </div>
+      {/* <h1 className="consulting">Publications</h1> */}
+      {/* <Footer /> */}
     </>
   );
 }
