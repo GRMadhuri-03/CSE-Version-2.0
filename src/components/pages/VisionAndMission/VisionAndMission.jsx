924930a158d6e6b86245cf4f1e3883ff6f9e17cf
GRMadhuri-03 <124073241+GRMadhuri-03@users.noreply.github.com>
2023-05-26 12:10:05 +0530

    1 file modified

--- src/components/pages/VisionAndMission/VisionAndMission.jsx
+++ src/components/pages/VisionAndMission/VisionAndMission.jsx
@@ -1,9 +1,59 @@
 import React from 'react';
 import './VisionAndMission.css';
-export default function VisionAndMission() {
+// export default function VisionAndMission() {
+//   return (
+//     <>
+//       <h1 className="consulting">VisionAndMission</h1>
+//     </>
+//   );
+// }
+import React from 'react';
+
+const AboutUs = () => {
   return (
-    <>
-      <h1 className="consulting">VisionAndMission</h1>
-    </>
+    <div>
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <h2>History</h2>
+            </div>
+            <div className="col-md-6">
+              <p>Content for the history section goes here...</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Mission</h2>
+              <p>Content for the mission section goes here...</p>
+            </div>
+            <div className="col-md-6">
+              <img src="mission-image.jpg" alt="Mission" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img src="vision-image.jpg" alt="Vision" />
+            </div>
+            <div className="col-md-6">
+              <h2>Vision</h2>
+              <p>Content for the vision section goes here...</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
-}
+};
+
+export default AboutUs;
