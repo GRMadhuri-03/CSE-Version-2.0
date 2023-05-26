5e322e827643b6a36dcab78f3a1436aad0ba9851
GRMadhuri-03 <124073241+GRMadhuri-03@users.noreply.github.com>
2023-05-26 15:28:39 +0530

    1 file modified

--- src/components/pages/VisionAndMission/VisionAndMission.jsx
+++ src/components/pages/VisionAndMission/VisionAndMission.jsx
@@ -1,57 +1,9 @@
 import React from 'react';
 import './VisionAndMission.css';
-// export default function VisionAndMission() {
-//   return (
-//     <>
-//       <h1 className="consulting">VisionAndMission</h1>
-//     </>
-//   );
-// }
-const VisionMission = () => {
+export default function VisionAndMission() {
   return (
-    <div>
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <h2>History</h2>
-            </div>
-            <div className="col-md-6">
-              <p>Content for the history section goes here...</p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <h2>Mission</h2>
-              <p>Content for the mission section goes here...</p>
-            </div>
-            <div className="col-md-6">
-              <img src="mission-image.jpg" alt="Mission" />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="vision-image.jpg" alt="Vision" />
-            </div>
-            <div className="col-md-6">
-              <h2>Vision</h2>
-              <p>Content for the vision section goes here...</p>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+    <>
+      <h1 className="consulting">VisionAndMission</h1>
+    </>
   );
-};
-
-export default VisionMission;
+}
